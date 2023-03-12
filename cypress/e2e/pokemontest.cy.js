d954eedcc6340dbb04e4f0bb6fee4ae03ec55570
krummus <64259895+krummus@users.noreply.github.com>
2023-03-12 22:05:39 +1100

    working cypress

--- cypress/e2e/pokemontest.cy.js
+++ cypress/e2e/pokemontest.cy.js
@@ -4,4 +4,17 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('ivysaur loads', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+    cy.get('td').should('have.class', 'pokemon-stats-name').contains('speed')
+    cy.get('td').should('have.class', 'pokemon-stats-value').contains(60)
+    cy.get('td').should('have.class', 'pokemon-stats-value').contains(80)
+    cy.get('td').should('have.class', 'pokemon-stats-value').contains(63)
+    cy.get('td').should('have.class', 'pokemon-stats-value').contains(62)
+  })
 })
